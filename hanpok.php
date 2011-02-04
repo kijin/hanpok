e53bb0608cd9186cf7f00529ca363958566d12c5
Kijin Sung <kijin.sung@gmail.com>
2011-02-03 19:13:32 -0500

    Change license to LGPL

--- hanpok.php
+++ hanpok.php
@@ -8,7 +8,7 @@
  * @package    HanPok
  * @author     Kijin Sung <kijin.sung@gmail.com>
  * @copyright  (c) 2010-2011, Kijin Sung <kijin.sung@gmail.com>
- * @license    GPL v3 <http://www.opensource.org/licenses/gpl-3.0.html>
+ * @license    LGPL v3 <http://www.gnu.org/copyleft/lesser.html>
  * @link       http://github.com/kijin/hanpok
  * @version    0.1.4
  * 
@@ -17,16 +17,16 @@
  * Copyright (c) 2010-2011, Kijin Sung <kijin.sung@gmail.com>
  * 
  * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- * 
- * You should have received a copy of the GNU General Public License
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
  * ----------------------------------------------------------------------------
